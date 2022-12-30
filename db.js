de118c7c639a17a831e4a6ddaa6bf5440692724c
Akber Hussain <akber_leghari@hotmail.com>
2022-12-30 18:55:53 +0500

    add local server database

--- db.js
+++ db.js
@@ -10,11 +10,21 @@ const mysql = require('mysql2/promise');
 //   queueLimit: 0
 // })
 
+// module.exports = mysql.createPool({
+//   host: 'db4free.net',
+//   user: 'vrbo_user',
+//   password: '123abc..',
+//   database: 'vrbo_db',
+//   waitForConnections: true,
+//   connectionLimit: 20,
+//   queueLimit: 0
+// })
+
 module.exports = mysql.createPool({
-  host: 'db4free.net',
-  user: 'vrbo_user',
-  password: '123abc..',
-  database: 'vrbo_db',
+  host: 'localhost',
+  user: 'root',
+  password: 'Pass@1122',
+  database: 'vrbo',
   waitForConnections: true,
   connectionLimit: 20,
   queueLimit: 0
