9726d7e12d53eb712524558e5076e0c91b5f19da
Akber Hussain <akber_leghari@hotmail.com>
2022-12-30 19:13:16 +0500

    final add local db and remove unnecessary files

--- db.js
+++ db.js
@@ -1,25 +1,5 @@
 const mysql = require('mysql2/promise');
 
-// module.exports = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '123abc..',
-//   database: 'vrbo',
-//   waitForConnections: true,
-//   connectionLimit: 20,
-//   queueLimit: 0
-// })
-
-// module.exports = mysql.createPool({
-//   host: 'db4free.net',
-//   user: 'vrbo_user',
-//   password: '123abc..',
-//   database: 'vrbo_db',
-//   waitForConnections: true,
-//   connectionLimit: 20,
-//   queueLimit: 0
-// })
-
 module.exports = mysql.createPool({
   host: 'localhost',
   user: 'root',
